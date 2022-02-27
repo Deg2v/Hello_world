b28692e3d791fa9c1cd30fc5b2a9ab05fe4dc664
Deg2v <1311618499.com>
2022-02-27 19:36:50 +0800

    another

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-a=3
-b=5
-if [[ $a > 2 && $a < 4 ]]; then
-	# echo $b
-	if [[ $b < 6 || $b > 4 ]]; then
-		echo "hello"
-	fi
+
+g=8.17
+# g=9
+echo $g
+# if [[ $g > 7 && $g < 10 ]]; then
+if [[ $g > 8  &&  $g < 10 ]]; then
+# if [[ $g -gt 8  &&  $g -lt 10 ]]; then
+	echo "gggg"
 fi
 
 
+echo "----"
+# if [[ $g > 7 && $g < 10 ]]; then
+if [[ $g -gt 8  &&  $g -lt 10 ]]; then
+echo "ggggggggg"
+fi
